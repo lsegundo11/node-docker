7ed2a94bd41c7b73f42a8dc9a52bed1ae62de24f
Luis Fernandez <lsegundo11@gmail.com>
2023-11-09 23:45:21 -0600

    working on the redis session manager

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
+const mongoose = require("mongoose");
+const redis = require("redis");
+const session = require("express-session");
+let RedisStore = require("connect-redis");
+
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
+
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
 const connectWithRetry = () => {
-    mongoose
-        .connect(mongoURL)
-        .then(() => console.log("successfully connected to database"))
-        .catch((e) => {
-            console.log(e)
-            setTimeout(connectWithRetry, 5000)
-        });
-
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("successfully connected to database"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
 };
 
 connectWithRetry();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi there</h2>")
+  res.send("<h2>Hi there</h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.PORT || 3000;
 
--- models/postModel.js
+++ models/postModel.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        require: [true, "Post must have title"]
-    },
-    body: {
-        type: String,
-        require: [true, "post must have body"],
-    },
+  title: {
+    type: String,
+    required: [true, "Post must have title"],
+  },
+  body: {
+    type: String,
+    required: [true, "Post must have body"],
+  },
 });
 
 const Post = mongoose.model("Post", postSchema);
