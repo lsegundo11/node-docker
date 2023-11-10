146b81f823e601cc583b1b376f91a516aa8033c8
Luis Fernandez <lsegundo11@gmail.com>
2023-11-09 20:10:40 -0600

    first commit

---
+++ controllers/postController.js
@@ -0,0 +1,19 @@
+const Post = require("../models/postModel");
+
+exports.getAllPosts = async (req, res, next) => {
+
+    try{
+        const posts = await Post.find();
+        res.status(200).json({
+            status: 'success',
+            results: posts.length,
+            data: {
+                posts
+            },
+        });
+    } catch (e) {
+        res.status(400).json({
+            status: "fail",
+        });
+    }    
+};
